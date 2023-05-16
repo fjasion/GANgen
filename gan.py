51febd4eb5def41c3f0b12332a05cadfbf5006e6
fjasion <filip.jasionowicz@student.uj.edu.pl>
2023-05-16 17:14:26 +0200

    Recreates single element

--- gan.py
+++ gan.py
@@ -5,21 +5,23 @@ import random
 from utils import show_img
 
 class GAN:
-    def __init__(self,data):
-        self.D = Discriminator()
-        self.G = Generator()
+    def __init__(self,data,noise_dim=256):
+        self.noise_dim = noise_dim
+        self.D = Discriminator(0.001)
+        self.G = Generator(0.01,noise_dim)
         self.data = data
     
     def train(self,iters = 100, K = 1):
         for it in range(iters):
-            if it % 10000 == 0:
-                show_img(self.G.generate(np.random.normal(size=32)))
+            if it % 1000 == 0:
+                #show_img(self.G.generate(np.random.normal(size=self.noise_dim)))
+                print(it)
             for k in range(K):
-                z = np.random.normal(size=32)
+                z = np.random.normal(size=self.noise_dim)
                 x = random.choice(self.data)
                 self.D.backprop(x,1)
                 self.D.backprop(self.G.generate(z),0)
-            z = np.random.normal(size=32)
+            z = np.random.normal(size=self.noise_dim)
             self.G.backprop(z,self.D)
     def generate(self):
-        return self.G.generate(np.random.normal(size=32))
+        return self.G.generate(np.random.normal(size=self.noise_dim))
